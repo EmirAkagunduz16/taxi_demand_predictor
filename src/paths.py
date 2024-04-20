7516fab3488d638f7157d6dce96f2d9cae41a8c4
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2024-04-20 17:02:23 +0300

    Fix line endings

---
+++ src/paths.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import os
+
+PARENT_DIR = Path(__file__).parent.resolve().parent
+DATA_DIR = PARENT_DIR / 'data'
+RAW_DATA_DIR =  PARENT_DIR / 'data' / 'raw'
+TRANSFORMED_DATA_DIR = PARENT_DIR / 'data' / 'transformed'
+
+if not Path(DATA_DIR).exists():
+  os.nkdir(DATA_DIR)
+
+if not Path(RAW_DATA_DIR).exists():
+  os.nkdir(RAW_DATA_DIR)
+
+if not Path(TRANSFORMED_DATA_DIR).exists():
+  os.nkdir(TRANSFORMED_DATA_DIR)
