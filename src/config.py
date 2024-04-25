af9d06a5c34b92115ed4923e19307ba04a61ba24
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2024-04-25 10:10:17 +0300

    Inference and training pipeline added!

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ from src.paths import PARENT_DIR
 # load key-value pairs from .env file located in the parent directory
 load_dotenv(PARENT_DIR / '.env')
 
-HOPSWORKS_PROJECT_NAME = 'taxi_demand'
+HOPSWORKS_PROJECT_NAME = 'taxi_demand_service'
 try:
     # HOPSWORKS_PROJECT_NAME = os.environ['HOPSWORKS_PROJECT_NAME']
     HOPSWORKS_API_KEY = os.environ['HOPSWORKS_API_KEY']
